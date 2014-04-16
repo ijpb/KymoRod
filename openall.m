5c12e9e6a2eb3a2737db0272fe246336140c7f29
lucien fregosi <lucienfregosi@gmail.com>
2014-04-16 15:15:26 +0200

    first comment add

--- openall.m
+++ openall.m
@@ -1,6 +1,16 @@
 function col=openall(color)
+%OPENALL Open all files of the directory 'r' for red, 'g' for green or 'b' for blue
+%Images must be in one color in one bit.
+%
+% Returns col, an array with all the pictures
+% ------
+% Author: Renaud Bastien
+% e-mail: rbastien@seas.harvard.edu
+% Created: 2012-03-03,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2010 INRA - Cepia Software Platform.
 
-
+%   HISTORY
+%   2013-04-16 : Add comments about the file
 N=dir(strcat(color,'*'));
 col=cell(length(N),1);
 
