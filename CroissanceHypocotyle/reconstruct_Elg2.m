62f3aca9a0a7b8430884a7c4e6b94277121d3695
dlegland <david.legland@grignon.inra.fr>
2014-10-29 13:01:25 +0100

    start code cleanup in reconstruct_Elg2.m

--- CroissanceHypocotyle/reconstruct_Elg2.m
+++ CroissanceHypocotyle/reconstruct_Elg2.m
@@ -1,6 +1,9 @@
-function [Ax]=reconstruct_Elg2(nx,varargin)
+function [Ax] = reconstruct_Elg2(nx, varargin)
 %RECONSTRUCT_ELG2 Construct kymograph
-%[Ax]=reconstruct_Elg2(nx,varargin)
+%
+% C2 = reconstruct_Elg2(nx, C);
+% C2 = reconstruct_Elg2(nx, C, Sa);
+% Reconstruct an image from a list of signals
 %
 % nx : number of points on which we resample the skeleton
 % varargin{1} : data who become a kymograph
@@ -21,47 +24,68 @@ function [Ax]=reconstruct_Elg2(nx,varargin)
 %   2014-04-16 : Add comments about the file
 
 
-if length(varargin)==2
-    S=varargin{2};
-    A=varargin{1};
+if length(varargin) == 1
+    signalList = varargin{1};
+    nSignals = length(signalList);
+    S = cell(nSignals, 1);
+    A = cell(nSignals, 1);
+
+    for k = 1:nSignals;
+        signal = signalList{k};
+        if ~isempty(signal)
+            S{k} = signal(:, 1);
+            A{k} = signal(:, 2);
+        else
+            S{k} = 0;
+            A{k} = 0;
+        end
+    end
+    
+elseif length(varargin) == 2
+    % first input is signal list, second input is list of abscissa
+    A = varargin{1};
+    S = varargin{2};
 end
 
-for k=1:length(varargin{1});
-    
-    if length(varargin)==1
-        S{k}=varargin{1}{k}(:,1);
-        A{k}=varargin{1}{k}(:,2);
-    end
-    if isempty(S{k})==1
-        S{k}=0;
-        A{k}=0;
-    end
-    Smin(k,1)=S{k}(1);
-    Smax(k,1)=S{k}(end);
-    
-end
-T=length(S);
-L=max(Smax);
-DL=L./nx;
+% number of signals, of frames
+nSignals = length(A);
 
-Ax=nan.*zeros(nx,T);
-for k=1:T
+% keep min and max abscissa
+Smin = zeros(nSignals, 1);
+Smax = zeros(nSignals, 1);
+for k = 1:nSignals;
+    Smin(k,1) = S{k}(1);
+    Smax(k,1) = S{k}(end);
+end
+
+
+L = max(Smax);
+DL = L ./ nx;
+
+% allocate memory for result image
+Ax = NaN * ones(nx, nSignals);
+
+% iterate on frames
+for k = 1:nSignals
     
-    if length(A{k})>10
+    % process only signals with enough data
+    if length(A{k}) > 10
         
-        for j=1+round(Smin(k)./DL):round(Smax(k)./DL)
-            
-            X=j.*DL;
-            posmin=find(S{k}<X,1,'last');
-            posmax=find(S{k}>X,1,'first');
+        % distribute some points
+        for j = 1+round(Smin(k)./DL):round(Smax(k)./DL)            
+            % convert to curvilinear abscissa
+            X = j * DL;
+            posmin = find(S{k} < X, 1, 'last');
             if isempty(posmin)
-                posmin=1;
+                posmin = 1;
             end
+            posmax = find(S{k} > X, 1, 'first');
             if isempty(posmax)
-                posmax=length(S{k});
+                posmax = length(S{k});
             end
             
-            Ax(j,k)=...
+            % linear interpolation ?
+            Ax(j, k) = ...
                 -(A{k}(posmax).*(X-S{k}(posmax))-A{k}(posmin).*(X-S{k}(posmin)))...
                 ./(S{k}(posmax)-S{k}(posmin));
             
