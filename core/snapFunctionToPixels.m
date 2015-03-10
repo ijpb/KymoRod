e23896e38986ee344905773ea8e971bc30d93d07
dlegland <david.legland@grignon.inra.fr>
2015-03-10 09:26:52 +0100

    function snapFunctionToPixels.m now returns sorted results

--- core/snapFunctionToPixels.m
+++ core/snapFunctionToPixels.m
@@ -1,11 +1,11 @@
 function [px, py, res] = snapFunctionToPixels(image, curve, S)
 %FUNCTIONTOIMAGE find pixel positions of a curve with values
 %
-%   [PX, PY, S2] = functionToImage(image, curve, S)
+%   [PX, PY, S2] = functionToImage(IMG, CURVE, S)
 %   
 %   Input arguments:
-%   image   an image, used to determine the size of the result
-%   curve   the skeleton, as a N-by-2 array of corodinates
+%   IMG     an image, used to determine the size of the result
+%   CURVE   the skeleton, as a N-by-2 array of coordinates
 %   S       curvilinear abscissa, as a N-by-1 array
 %
 %   Output arguments:
@@ -20,10 +20,10 @@ function [px, py, res] = snapFunctionToPixels(image, curve, S)
 % Created: 2012-03-03,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2012 INRA - Cepia Software Platform.
 
-% determine size of result image
+% size of result image
 dim  = size(image);
 
-% extract curve coordinates
+% extract curve coordinates, and convert to pixel coordinates
 px = round(curve(:, 1));
 py = round(curve(:, 2));
 
@@ -36,14 +36,19 @@ S  = S(insideFlag);
 % compute linear indices in image 
 inds = sub2ind(dim, py, px);
 
-% compute average for each unique index
-[uninds, ia, ic] = unique(inds);
+% indentify unique positions
+[uninds, ia, ic] = unique(inds); %#ok<ASGLU>
 px = px(ia);
 py = py(ia);
 
+% compute average value of function for each unique index
 res = zeros(length(uninds), 1);
-
 for i = 1:length(uninds)
     uni = uninds(i);
     res(i) = mean(S(inds == uni));
 end
+
+% sort results in ascending order of function value
+[res, inds] = sort(res);
+px = px(inds);
+py = py(inds);
