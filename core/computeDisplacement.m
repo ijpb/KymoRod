0c32b72963a62f6ac777cfc5773639f98de1049b
Renaud Bastien <RnoB@users.noreply.github.com>
2016-11-24 11:15:59 +0100

    Display tracking algorithm
    put back the display image to track what the elongation algorithm is doing

--- core/computeDisplacement.m
+++ core/computeDisplacement.m
@@ -29,7 +29,8 @@ function E = computeDisplacement(SK1, SK2, S1, S2, pic1, pic2, ws, L)
 % e-mail: rbastien@seas.harvard.edu
 % Created: 2012-03-03,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2012 INRA - Cepia Software Platform.
-
+% % flag for displaying evolution of computation or not
+ disp = 0;
 dim = size(pic1);
 
 % identify in each image the pixels containing a portion of skeleton
@@ -43,7 +44,7 @@ E = zeros(length(x1), 2);
 a = 0;
 
 % apply Particle Image Velocimetry on each point of the skeleton
-for k = 1:length(x1)
+for k = 1:100:length(x1)
 	% image indices of current point
     i = y1(k);
     j = x1(k);
@@ -54,16 +55,16 @@ for k = 1:length(x1)
     end
     
     % get small image around current point of first skeleton
-    w1 = double(pic1(i-ws:i+ws, j-ws:j+ws));
+    w1a = double(pic1(i-ws:i+ws, j-ws:j+ws));
     
     % compute PIV only if variability in window is sufficient
-    V = std2(w1);
+    V = std2(w1a);
     if V <= 1
         continue;
     end
         
     % transform to vector, and remove mean
-    w1 = w1(:) - mean(w1(:));
+    w1 = w1a(:) - mean(w1a(:));
     
     % identify positions in second image with similar curvilinear abscissa
     inds = find( abs(S2bis - S1bis(k)) < L & S2bis > 0);
@@ -91,10 +92,10 @@ for k = 1:length(x1)
         v = x2(l);
         
         % get small image around current point in second skeleton
-        w2 = double(pic2(u-ws:u+ws, v-ws:v+ws));
+        w2a = double(pic2(u-ws:u+ws, v-ws:v+ws));
         
         % transform to vector, and remove mean
-        w2 = w2(:) - mean(w2(:));
+        w2 = w2a(:) - mean(w2a(:));
                 
         % compute image correlation between the two thumbnails.
         % the result is the correlation value, and the difference in
@@ -102,6 +103,9 @@ for k = 1:length(x1)
         % to the displacement
         Corr(l, 1) = S2k(l) - S1bis(k);
         Corr(l, 2) = sum(w1 .* w2) / sqrt(sum(w1 .* w1) * sum(w2 .* w2));
+        
+        if disp==1;clear figure;subplot(2,2,1); imagesc(w1a);subplot(2,2,2); imagesc(w2a);drawnow; end;
+        %
     end
 			
     % find the index of maximum correlation by sorting the Corr array
@@ -112,6 +116,20 @@ for k = 1:length(x1)
     a = a + 1;
     E(a, 1) = S1bis(k);
     E(a, 2) = Corr(Corry,1);
+    
+    
+ 			if disp == 1
+ 				subplot(2,2,3);
+				hold off;
+				plot(Corr(:,1), Corr(:,2), 'go', Corr(Corry,1), Corr(Corry,2), 'r+'); 
+ 				drawnow;
+ 			end
+ 			
+ 			if disp == 1
+ 				subplot(2,2,4); hold off;
+ 				plot(E(:,1), E(:,2), 'ro'); 
+ 				drawnow;
+ 			end
 end
 
 % keep and sort relevant results
